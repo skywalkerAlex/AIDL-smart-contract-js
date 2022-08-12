eb6545a5c4a2c60432e3269222e80b40f5ed0d6d
Alex Liakopoulos <alexdim.liak@gmail.com>
2022-08-12 22:12:59 +0300

    initial installments (#1)

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect , useState } from 'react';
+import twitterLogo from './assets/twitter-logo.svg';
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+import Card  from 'react-bootstrap/Card';
+
+import idl from './idl.json';
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Program, Provider, web3 } from '@project-serum/anchor';
+import kp from './keypair.json'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
